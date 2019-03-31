e9238dca83f45f3d4b6facc9659296329184427a
Anderson Gaspar <andersongasparm@gmail.com>
2019-03-30 23:22:21 -0300

    sec, min, hora feito

--- src/main/java/poo/Principal.java
+++ src/main/java/poo/Principal.java
@@ -2,6 +2,14 @@ package poo;
 
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Olá");
+       //System.out.println("Olá");
+
+        Relogio relogio = new Relogio();
+
+        relogio.setSec(70);
+        relogio.setMin(10);
+        relogio.setHora(10);
+
+        System.out.println(relogio.toString());
     }
 }
--- src/main/java/poo/Relogio.java
+++ src/main/java/poo/Relogio.java
@@ -2,5 +2,48 @@ package poo;
 
 public class Relogio {
 
+    private int sec;
+    private int min;
+    private int hora;
+
+    public boolean setSec(int sec){
+        if ((sec >= 0)&&(sec <= 60)){
+            this.sec = sec;
+            return true;
+        }else{
+            this.sec = 0;
+            return false;
+        }
+    }
+
+    public boolean setMin(int min) {
+        if ((min >= 0) && (min <= 60)) {
+            this.min = min;
+            return true;
+        }else{
+            this.min = 0;
+            return false;
+        }
+    }
+
+    public boolean setHora(int hora) {
+        if ((hora >= 0) && (hora < 24)) {
+            this.hora = hora;
+            return true;
+        }else{
+            this.hora = 0;
+            return false;
+        }
+    }
+
+    public String toString(){
+        return hora +":"+ min +":"+ sec;
+    }
+
+    public void toFormat(int x){
+        if (x == 0){
+            x = 00;
+        }
+    }
 
 }
